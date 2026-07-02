5ca061177ae97bd83030fc69dde11554cbbaa0e6
juuanmontoya <juuanmontoya@gmail.com>
2026-07-02 10:51:49 -0500

    Feat:layout

--- src/app/finanzas/layout.tsx
+++ src/app/finanzas/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Finanzas",
 };
+
+export default function FinanzasLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
+}
